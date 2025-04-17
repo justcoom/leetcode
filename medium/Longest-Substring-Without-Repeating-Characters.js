7f779a03d7d34c5375256259d824126018de6cc0
Valery Bruner <vbruner11@mail.ru>
2025-04-17 18:50:52 +0700

    use Set and arr methods

---
+++ medium/Longest-Substring-Without-Repeating-Characters.js
@@ -0,0 +1,64 @@
+// Given a string s, find the length of the longest substring without duplicate characters.
+//
+//
+//
+//     Example 1:
+//
+// Input: s = "abcabcbb"
+// Output: 3
+// Explanation: The answer is "abc", with the length of 3.
+// Example 2:
+//
+// Input: s = "bbbbb"
+// Output: 1
+// Explanation: The answer is "b", with the length of 1.
+// Example 3:
+//
+// Input: s = "pwwkew"
+// Output: 3
+// Explanation: The answer is "wke", with the length of 3.
+// Notice that the answer must be a substring, "pwke" is a subsequence and not a substring.
+
+const lengthOfLongestSubstring = function (s) {
+    const resArr = []
+    let maxLength = 0
+    for (let i = 0; i < s.length; i++) {
+        if (resArr.includes(s[i])) {
+            const repeatEl = resArr.indexOf(s[i])
+            resArr.splice(0, repeatEl + 1)
+            resArr.push(s[i])
+        } else {
+            resArr.push(s[i])
+            if (resArr.length > maxLength) {
+                maxLength = resArr.length
+            }
+        }
+    }
+    return maxLength
+};
+
+const lengthOfLongestSubstring2 = function(s) {
+    let left = 0;
+    let maxLength = 0;
+    let charSet = new Set();
+
+    for (let right = 0; right < s.length; right++) {
+        while (charSet.has(s[right])) {
+            charSet.delete(s[left]);
+            left++;
+        }
+
+        charSet.add(s[right]);
+        maxLength = Math.max(maxLength, right - left + 1);
+    }
+
+    return maxLength;
+};
+
+console.log(lengthOfLongestSubstring2("abcabcbb"))
+console.log(lengthOfLongestSubstring("a"))
+console.log(lengthOfLongestSubstring("aub"))
+console.log(lengthOfLongestSubstring(""))
+console.log(lengthOfLongestSubstring(" "))
+
+console.log(lengthOfLongestSubstring("nfpdmpi"))
